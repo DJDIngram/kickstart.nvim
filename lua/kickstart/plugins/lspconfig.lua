fb69e16ba564afaf5c3361d8e345960d45ff8bd9
Dan Ingram <danieljdingram@gmail.com>
2026-05-14 19:23:08 +0100

    go setup, show hidden from neotree

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -217,7 +217,7 @@ return {
         prismals = {},
         -- eslint_d = {},
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
